5b58355460d87989c6e60580df0b848cc70037a2
Mohammad Omer <mohammad.omer@analyticsseo.com>
2015-02-13 12:46:02 +0000

    functional testing

--- tests/functional/functionaltesting.php
+++ tests/functional/functionaltesting.php
@@ -4,111 +4,7 @@
 #   Tests that should pass
 ##################################################################################
 $tests = array(
- "google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'google',
-           'phrase' => 'cricket world cup',
-            'universal' => 0,
-        )
-    ),
-	 "google global" => array(
-    "request" => array(
-      'region'=>'fr',
-          'search_engine' => 'google',
-           'phrase' => 'voulez-vous une tasse de thé ou une tasse de café ou une tasse de boisson gazeuse?',
-            'language' => 'fr',
-        )
-    ),
-	"google global" => array(
-    "request" => array(
-      'region'=>'hk',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-TW',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'cn',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-CN',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'tw',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-TW',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'dz',
-          'search_engine' => 'google',
-           'phrase' => 'هل تريد فنجانا من الشاي أو فنجان من القهوة أو كوب من المشروبات الغازية؟',
-		   'language' => 'ar',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'il',
-          'search_engine' => 'google',
-           'phrase' => 'אתה רוצה כוס התה או כוס קפה או כוס שתייה קלה?',
-		   'language' => 'he',
-			)
-        ),
-				"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'bing',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),	
-					"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'yahoo',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),	
-		"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'yandex',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'hu',
-          'search_engine' => 'google',
-           'phrase' => 'kérsz egy csésze tea vagy egy csésze kávé vagy egy pohár üdítő?',
-		   'language' => 'hu',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'pt',
-          'search_engine' => 'google',
-           'phrase' => 'você quer uma xícara de chá ou uma xícara de café ou um copo de refrigerante?',
-		   'language' => 'pt-PT',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'kr',
-          'search_engine' => 'google',
-           'phrase' => '당신은 차 한 잔 또는 커피 한 잔 또는 소프트 드링크 한 잔할까요?',
-		   'language' => 'ko',
-			)
-        ),
-		"google global" => array(
+ 	"google global" => array(
     "request" => array(
       'region'=>'in',
           'search_engine' => 'google',
@@ -116,21 +12,6 @@ $tests = array(
 		   'language' => 'bn',
 			)
         ),
-);
-
-##################################################################################
-#   Tests that should return an api error message
-##################################################################################
-$errorTests = array(
-   "invalid language" => array(
-      "request" => array(
-           'region'=>'global',
-           'search_engine' => 'google',
-           'phrase' => 'abc',
-            'universal' => 0,
-            'language' => 'asd',
-        )
-    ),
 );
 
 ##################################################################################
@@ -164,7 +45,7 @@ $auth->setApiSecret(API_SECRET);
 $auth->setSalt(SALT);
 
 $serps = new Aseo\Api\V3\Serps\SerpsApiClient($guzzle, $auth);
-$serps->debug = false;
+$serps->debug = TRUE;
 
 
 
