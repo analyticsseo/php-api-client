8df1d87a5a1a96cf3b23922c40fe0bdf87b99be7
Nuno Costa <nuno.costa@analyticsseo.com>
2015-02-19 12:04:36 +0000

    cleanup serps.php funcitonal testing file

--- tests/functional/serps.php
+++ tests/functional/serps.php
@@ -4,118 +4,128 @@
 #   Tests that should pass
 ##################################################################################
 $tests = array(
- "google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'google',
-           'phrase' => 'cricket world cup',
-            'universal' => 0,
-        )
+    "google global" => array(
+        "request" => array(
+          'region'=>'global',
+              'search_engine' => 'google',
+               'phrase' => 'cricket world cup',
+                'universal' => 0,
+            )
     ),
-	 "google global" => array(
-    "request" => array(
-      'region'=>'fr',
-          'search_engine' => 'google',
-           'phrase' => 'voulez-vous une tasse de thé ou une tasse de café ou une tasse de boisson gazeuse?',
+
+    "google fr" => array(
+        "request" => array(
+           'region'=>'fr',
+            'search_engine' => 'google',
+            'phrase' => 'voulez-vous une tasse de thé ou une tasse de café ou une tasse de boisson gazeuse?',
             'language' => 'fr',
         )
     ),
-	"google global" => array(
-    "request" => array(
-      'region'=>'hk',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-TW',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'cn',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-CN',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'tw',
-          'search_engine' => 'google',
-           'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
-		   'language' => 'zh-TW',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'dz',
-          'search_engine' => 'google',
-           'phrase' => 'هل تريد فنجانا من الشاي أو فنجان من القهوة أو كوب من المشروبات الغازية؟',
-		   'language' => 'ar',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'il',
-          'search_engine' => 'google',
-           'phrase' => 'אתה רוצה כוס התה או כוס קפה או כוס שתייה קלה?',
-		   'language' => 'he',
-			)
-        ),
-				"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'bing',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),	
-					"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'yahoo',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),	
-		"google global" => array(
-    "request" => array(
-      'region'=>'global',
-          'search_engine' => 'yandex',
-           'phrase' => 'best place to take selfie in London',
-		   'language' => 'en',
-			)
-        ),
-			"google global" => array(
-    "request" => array(
-      'region'=>'hu',
-          'search_engine' => 'google',
-           'phrase' => 'kérsz egy csésze tea vagy egy csésze kávé vagy egy pohár üdítő?',
-		   'language' => 'hu',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'pt',
-          'search_engine' => 'google',
-           'phrase' => 'você quer uma xícara de chá ou uma xícara de café ou um copo de refrigerante?',
-		   'language' => 'pt-PT',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'kr',
-          'search_engine' => 'google',
-           'phrase' => '당신은 차 한 잔 또는 커피 한 잔 또는 소프트 드링크 한 잔할까요?',
-		   'language' => 'ko',
-			)
-        ),
-		"google global" => array(
-    "request" => array(
-      'region'=>'in',
-          'search_engine' => 'google',
-           'phrase' => 'আপনি এক কাপ চা বা কফি এক কাপ বা নরম পানীয় একটি কাপ চান?',
-		   'language' => 'bn',
-			)
-        ),
+
+    "google hk" => array(
+        "request" => array(
+            'region'=>'hk',
+            'search_engine' => 'google',
+            'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
+            'language' => 'zh-TW',
+        )
+    ),
+    "google cn" => array(
+        "request" => array(
+            'region'=>'cn',
+            'search_engine' => 'google',
+            'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
+            'language' => 'zh-CN',
+        )
+    ),
+
+    "google tw" => array(
+        "request" => array(
+            'region'=>'tw',
+            'search_engine' => 'google',
+            'phrase' => '你要一杯茶或一杯咖啡或一杯饮料吗？',
+            'language' => 'zh-TW',
+        )
+    ),
+    "google dz" => array(
+        "request" => array(
+            'region'=>'dz',
+            'search_engine' => 'google',
+            'phrase' => 'هل تريد فنجانا من الشاي أو فنجان من القهوة أو كوب من المشروبات الغازية؟',
+            'language' => 'ar',
+        )
+    ),
+
+    "google il" => array(
+        "request" => array(
+            'region'=>'il',
+            'search_engine' => 'google',
+            'phrase' => 'אתה רוצה כוס התה או כוס קפה או כוס שתייה קלה?',
+            'language' => 'he',
+        )
+    ),
+
+    "bing global" => array(
+        "request" => array(
+            'region'=>'global',
+            'search_engine' => 'bing',
+            'phrase' => 'best place to take selfie in London',
+            'language' => 'en',
+        )
+    ),
+    "yahoo global" => array(
+        "request" => array(
+            'region'=>'global',
+            'search_engine' => 'yahoo',
+            'phrase' => 'best place to take selfie in London',
+            'language' => 'en',
+        )
+    ),
+
+    "yandex global" => array(
+        "request" => array(
+            'region'=>'global',
+            'search_engine' => 'yandex',
+            'phrase' => 'best place to take selfie in London',
+            'language' => 'en',
+        )
+    ),
+
+    "google hu" => array(
+        "request" => array(
+            'region'=>'hu',
+            'search_engine' => 'google',
+            'phrase' => 'kérsz egy csésze tea vagy egy csésze kávé vagy egy pohár üdítő?',
+            'language' => 'hu',
+        )
+    ),
+
+    "google pt" => array(
+        "request" => array(
+            'region'=>'pt',
+            'search_engine' => 'google',
+            'phrase' => 'você quer uma xícara de chá ou uma xícara de café ou um copo de refrigerante?',
+            'language' => 'pt-PT',
+        )
+    ),
+
+    "google kr" => array(
+        "request" => array(
+            'region'=>'kr',
+            'search_engine' => 'google',
+            'phrase' => '당신은 차 한 잔 또는 커피 한 잔 또는 소프트 드링크 한 잔할까요?',
+            'language' => 'ko',
+        )
+    ),
+
+    "google in" => array(
+        "request" => array(
+            'region'=>'in',
+            'search_engine' => 'google',
+            'phrase' => 'আপনি এক কাপ চা বা কফি এক কাপ বা নরম পানীয় একটি কাপ চান?',
+            'language' => 'bn',
+        )
+    ),
 );
 
 ##################################################################################
@@ -123,12 +133,12 @@ $tests = array(
 ##################################################################################
 $errorTests = array(
    "invalid language" => array(
-      "request" => array(
+        "request" => array(
            'region'=>'global',
            'search_engine' => 'google',
            'phrase' => 'abc',
-            'universal' => 0,
-            'language' => 'asd',
+           'universal' => 0,
+           'language' => 'asd',
         )
     ),
 );
@@ -203,7 +213,7 @@ foreach ($tests as $testName => $testData) {
             }
 
             echo "[OK]\n";
-            break;
+            break 1;
         }
 
 
@@ -231,7 +241,7 @@ foreach ($errorTests as $testName => $testData) {
 
             if (array_key_exists('error', $fetchJobResponse)) {
                 echo "[OK]";
-                echo "\t ==> " . $fetchJobResponse['error'] . "\n\n";
+                // echo "\t ==> " . $fetchJobResponse['error'] . "\n\n";
                 break;
             }
 
@@ -244,7 +254,9 @@ foreach ($errorTests as $testName => $testData) {
 
 
     } catch (\Exception $e) {
-            echo "[OK??]\n";
+            echo "[??]\n";
             echo "\t ==> " . $e->getMessage() . "\n\n";
     }
 }
+
+echo "\n";
